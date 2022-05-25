c0093176bed12f26d0f185942c8d38adac0e1306
Nayuta <naayudan@gmail.com>
2022-05-25 09:01:32 -0300

    Primeiro Console | Arrays

---
+++ Colecoes/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Colecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            
+
+            // Array multidimencional
+
+            // int[,] matriz = new int[4, 2]
+            // {
+            //     { 8,  8 },
+            //     { 10, 20 },
+            //     { 50, 100 },
+            //     { 90, 200 }
+            // };
+
+            // for (int i = 0; i < matriz.GetLength(0); i++)
+            // {
+            //     for (int j = 0; j < matriz.GetLength(1); j++)
+            //     {
+            //         System.Console.WriteLine(matriz[i,j]);
+            //     }
+            // }
+
+            // foreach (var item in matriz)
+            // {
+            //     Console.WriteLine(item);
+            // }
+
+            // Array de inteiros
+            // int[] arrayInteiros = new int[3];
+
+            // arrayInteiros[0] = 10;
+            // arrayInteiros[1] = 20;
+            // arrayInteiros[2] = int.Parse("30");
+            // arrayInteiros[3] = 30;
+
+            // System.Console.WriteLine("Array pelo for");
+            // for (int i = 0; i < arrayInteiros.Length; i++)
+            // {
+            //     System.Console.WriteLine(arrayInteiros[i]);
+            // }
+
+            // System.Console.WriteLine("Array pelo foreach");
+            // foreach (int item in arrayInteiros)
+            // {
+            //     System.Console.WriteLine(item);
+            // }
+
+
+
+        }
+    }
+}
